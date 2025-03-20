32ef2c98d328bc320d43fa526e1473a55660d79d
mariohmis <138615425+MarioHMis@users.noreply.github.com>
2025-03-20 12:44:15 -0600

    adding the entities and repository to orders

---
+++ orders-service/src/main/java/com/marwarecode/orders_service/model/entities/OrderItems.java
@@ -0,0 +1,22 @@
+package com.marwarecode.orders_service.model.entities;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderItems {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String sku;
+    private Double pricel;
+    private Long quantity;
+}
---
+++ orders-service/src/main/java/com/marwarecode/orders_service/repositories/OrderRepository.java
@@ -0,0 +1,4 @@
+package com.marwarecode.orders_service.repositories;
+
+public class OrderRepository {
+}
